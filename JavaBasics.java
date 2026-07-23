cfdc3678804e739572852cae565cd1e5dcc15e40
Shreya Sutar <shreyaasutar14@gmail.com>
2026-07-23 09:51:59 +0530

    Update JavaBasics.java

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,5 +1,13 @@
+//Boilerplate code
+//output in java: print, println, \n
+
 public class JavaBasics{
     public static void main(String args[]){
         System.out.print("Hello shreya");
+        System.out.println("Hello shreya");  //line space
+        System.out.println("Hello shreya");
+        System.out.print("Hello shreya\n");
     }
 }
+
+
