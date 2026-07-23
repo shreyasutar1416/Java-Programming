f0121f7c380ff3b11f4dd3266db4ad2cd2ca1234
Shreya Sutar <shreyaasutar14@gmail.com>
2026-07-23 11:28:55 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,10 @@
+//Print pattern
+
+public class pattern{
+    public static void main(String args[]){
+        System.out.println("****");  
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
