40d52ac82d32c339a9ed1be4ca14acdc34587045
Shreya Sutar <shreyaasutar14@gmail.com>
2026-07-23 11:29:54 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes{
+    public static void main(String args[]){
+        byte b = 8;   //1 byte
+        System.out.println(b);  
+
+        char ch = 'a';   //2 bytes
+        System.out.println(ch);
+
+        boolean var = false;  //1 byte
+        System.out.println(var);
+
+        float price = 10.5f;   //4 bytes
+        System.out.println(price);
+
+        int number = 25;      //4 bytes
+        System.out.println(number);
+
+        //long                //8 bytes
+        //double              //8 bytes
+
+        short n = 240;        //2 bytes
+        System.out.println(n);
+    }
+}
