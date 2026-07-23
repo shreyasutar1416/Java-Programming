d30094537bae30d75948cac9023a6981f51edac3
Shreya Sutar <shreyaasutar14@gmail.com>
2026-07-23 10:56:18 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Input{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+        // float price = sc.nextFloat();
+        // System.out.println(price);
+
+        // double d = sc.nextDouble();
+        // System.out.println(d);
+
+        //Sum of a and b
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // System.out.println(sum);
+
+        //Product of a and b
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int product = a * b;
+        // System.out.println(product);
+
+        //Area of circle
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+    }
+}
